7cc8e0b3352895926f3c66a5c017347d99b24ec2
sfrabby <fazlerabby6732@gmail.com>
2026-01-16 23:59:46 +0600

    done

--- lib/view/product_screen/ui.dart
+++ lib/view/product_screen/ui.dart
@@ -1,6 +1,8 @@
 import 'package:e_buy/constants/Colors.dart';
 import 'package:e_buy/constants/SizeBox.dart';
+import 'package:e_buy/utils/widget/commonButton.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 
 class ProductScreen extends StatelessWidget {
   const ProductScreen({super.key});
@@ -10,7 +12,10 @@ class ProductScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: PrimaryColor,
-        title: Text("Product Details", style: TextStyle(color: BgColor, fontSize: 22),),
+        title: Text(
+          "Product Details",
+          style: TextStyle(color: BgColor, fontSize: 22),
+        ),
         centerTitle: true,
       ),
       body: Column(
@@ -28,12 +33,19 @@ class ProductScreen extends StatelessWidget {
                     color: SecondaryColor,
                   ),
                 ),
+                SizedBox(height: spaceBtwSection),
+                Text("Mens Formal Shirt", style: TextStyle(fontSize: 20)),
+                Text("Price -5000", style: TextStyle(color: Colors.red)),
+                Text("Product Rating - 5.5", style: TextStyle(fontSize: 16)),
+                Text(
+                  "A shirt is a versatile upper-body garment, typically cloth, with sleeves, a neck opening, and often a collar and front buttons, made from materials like cotton, silk, or polyester, coming in styles from casual tees to formal dress shirts, designed for comfort, protection, and style. Descriptions highlight material,",
+                  style: TextStyle(fontSize: 16),
+                ),
                 SizedBox(height: spaceBtwSection,),
-                Text("Mens Formal Shirt", style: TextStyle(fontSize: 20),)
+                CommonButton(child: Text("Buy Now"), onTap: (){}, height: 50)
               ],
-
             ),
-          )
+          ),
         ],
       ),
     );
